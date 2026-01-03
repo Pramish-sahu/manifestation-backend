44de5629aeb3830bd610f1caaed3ebb4438ae1ad
Pramish-sahu <pramishkumar555@gmail.com>
2026-01-03 20:34:45 +0530

    Fix register route to return token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,10 +3,11 @@ import { loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-// router.post("/register", registerUser);
-router.post("/register", (req, res) => {
-  res.json({ ok: true });
-});
+router.post("/register", registerUser);
+
+// router.post("/register", (req, res) => {
+//   res.json({ ok: true });
+// });
 
 router.post("/login", loginUser);
 
